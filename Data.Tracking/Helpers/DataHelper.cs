64d3312d98026545e4abcf7fe2990651d3fb7651
Oliver Waits <owaits@gmail.com>
2021-01-15 15:08:16 +0000

    Add data helper that uses a regex to create a link for a raw URL.

---
+++ Data.Tracking/Helpers/DataHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Data.Tracking.Helpers
+{
+    public static class DataHelper
+    {
+        public static Regex urlMatch = new Regex(@"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public static string FormatHtmlLinks(string text)
+        {
+            return urlMatch.Replace(text, "<a target='_blank' href='$1'>$1</a>");
+        }
+
+    }
+}
