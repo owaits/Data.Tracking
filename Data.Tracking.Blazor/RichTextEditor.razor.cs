1a290825ec5f106f6eee594e60b41955d5a2f265
Oliver Waits <owaits@gmail.com>
2021-06-04 11:26:52 +0100

    Updated the RichTextEditor to use the full version that supports source editing. Moved the ckEditor js files into embedded resources so they can be loaded from library reference. Added a demo project that shows how to use the RichTextEditor and helps with testing.

---
+++ Data.Tracking.Blazor/RichTextEditor.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oarw.Data.Tracking.Blazor
+{
+    public partial class RichTextEditor: InputTextArea, IAsyncDisposable
+    {
+        [Inject]
+        public IJSRuntime JavaScript { get; set; }
+
+        string _id;
+
+        [Parameter]
+        public string Id
+        {
+            get => _id ?? $"CKEditor_{uid}";
+            set => _id = value;
+        }
+
+        readonly string uid = Guid.NewGuid().ToString().ToLower().Replace("-", "");
+
+        private Task<IJSObjectReference> _ckEditorModule;
+        private Task<IJSObjectReference> ckEditorModule => _ckEditorModule ??= JavaScript.InvokeAsync<IJSObjectReference>("import", "/_content/Oarw.Data.Tracking.Blazor/js/ckEditor/ckEditor-module.js?1").AsTask();
+
+        private IJSObjectReference editor = null;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                editor = await (await ckEditorModule).InvokeAsync<IJSObjectReference>("initialiseCKEditor", Id, DotNetObjectReference.Create(this));
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        [JSInvokable]
+        public Task EditorDataChanged(string data)
+        {
+           CurrentValue = data;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await(await ckEditorModule).InvokeAsync<IJSObjectReference>("disposeCKEditor", editor);
+        }
+    }
+}
---
+++ Data.Tracking.Blazor/wwwroot/js/ckEditor/ckEditor-module.js
@@ -0,0 +1,17 @@
+﻿//import './ckeditor.js';
+
+export async function initialiseCKEditor(elementId, dotNetReference) {
+    var editor = CKEDITOR.replace(elementId);
+
+    editor.on('change', function () {
+        let data = editor.getData();
+        dotNetReference.invokeMethodAsync('EditorDataChanged', data);
+    });
+
+    return editor;
+}
+
+export async function disposeCKEditor(editor) {
+    if (typeof (editor) != 'undefined' && editor != null)
+        editor.destroy();
+}
