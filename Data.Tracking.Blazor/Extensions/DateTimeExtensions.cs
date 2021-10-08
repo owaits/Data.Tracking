9c0b0c26fd5fd3cb5175a33d8f51e00a016c6023
Oliver Waits <owaits@gmail.com>
2021-10-08 10:45:27 +0100

    Add in nullable EndOfDay date extensions.

--- Data.Tracking.Blazor/Extensions/DateTimeExtensions.cs
+++ Data.Tracking.Blazor/Extensions/DateTimeExtensions.cs
@@ -19,6 +19,16 @@ namespace Oarw.Data.Tracking.Blazor.Extensions
         }
 
         /// <summary>
+        /// Gets the date time with the time set to the end of the current day specified by the date.
+        /// </summary>
+        /// <param name="date">The date to get the end of the day for.</param>
+        /// <returns>The end of the current day.</returns>
+        public static DateTime? EndOfDay(this DateTime? date)
+        {
+            return date?.Date.AddDays(1).AddTicks(-1);
+        }
+
+        /// <summary>
         /// Gets the date time with the time set to the start of the current day specified by the date.
         /// </summary>
         /// <param name="date">The date to get the start of the day for.</param>
@@ -26,6 +36,16 @@ namespace Oarw.Data.Tracking.Blazor.Extensions
         public static DateTime StartOfDay(this DateTime date)
         {
             return date.Date;
+        }
+
+        /// <summary>
+        /// Gets the date time with the time set to the start of the current day specified by the date.
+        /// </summary>
+        /// <param name="date">The date to get the start of the day for.</param>
+        /// <returns>The start of the current day.</returns>
+        public static DateTime? StartOfDay(this DateTime? date)
+        {
+            return date?.Date;
         }
     }
 }
