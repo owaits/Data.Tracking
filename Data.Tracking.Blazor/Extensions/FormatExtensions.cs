22e6af914b0b6a82edc2026d50b29f7efaf040fd
Oliver Waits <owaits@gmail.com>
2023-04-13 11:11:56 +0100

    Add page to add and edit the age restrictions.

---
+++ Data.Tracking.Blazor/Extensions/FormatExtensions.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Oarw.Data.Tracking.Blazor.Extensions
+{
+    public static class FormatExtensions
+    {
+        public static string FormatMoney(this decimal? value)
+        {
+            return (value != null ? ((decimal)value).FormatMoney() : "-");
+        }
+
+
+        public static string FormatMoney(this decimal value)
+        {
+            try
+            {
+                return "£" + value.ToString("n2");
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatMoney(this double value)
+        {
+            return ((decimal)value).FormatMoney();
+        }
+
+        public static string FormatPercentage(this decimal? value)
+        {
+            return (value != null ? ((decimal)value).FormatPercentage() : "-");
+        }
+
+
+
+        public static string FormatPercentage(this decimal value)
+        {
+            try
+            {
+                return Math.Round((value * 100), 0).ToString() + "%";
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatFrequency(this double? value)
+        {
+            return (value != null ? ((double)value).FormatFrequency() : "-");
+        }
+
+        public static string FormatFrequency(this double value)
+        {
+            try
+            {
+                return value.ToString("0.000") + "MHz";
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatWeight(this decimal? value)
+        {
+            return (value != null ? ((decimal)value).FormatWeight() : "-");
+        }
+
+        public static string FormatWeight(this decimal value)
+        {
+            try
+            {
+                return value.ToString() + "kg";
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatVolume(this decimal? value)
+        {
+            return (value != null ? ((decimal)value).FormatVolume() : "-");
+        }
+
+        public static string FormatVolume(this decimal value)
+        {
+            try
+            {
+                return (value / 1000000000).ToString("0.00") + "m³";
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatDimension(this decimal? value)
+        {
+            try
+            {
+                return (value != null ? ((decimal)value).ToString("0") + "mm" : "-");
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatAssetType(this int? value)
+        {
+            try
+            {
+                string[] typeNames = new string[] { "Fixed Asset(FA)", "Cost of Goods Sold(COGS)", "Stock", "Admin","Transport","Buildings", "IT","Marketing","Entertainment","Payroll"};
+                return (value != null && value > 0 ? typeNames[(int)value - 1] : "-");
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        /// <summary>
+        /// Formats the date for display in UI. Null datyes will be formated with dash.
+        /// </summary>
+        /// <param name="value">The date to be formatted.</param>
+        /// <returns>The formated date for display in UI.</returns>
+        public static string FormatDate(this DateTime? value, string format = null)
+        {
+            if (value == null)
+                return "-";
+            return ((DateTime)value).FormatDate(format);
+        }
+
+        public static string FormatDate(this DateTime value, string format = null)
+        {
+            try
+            {
+                return value.ToString(format ?? "dd/MM/yy");
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatTime(this DateTime value, string format = null)
+        {
+            try
+            {
+                return value.ToString(format ?? "HH:mm");
+            }
+            catch (Exception ex)
+            {
+                return $"[error: {ex.Message}]";
+            }
+        }
+
+        public static string FormatTimeSpan(this TimeSpan? value)
+        {
+            if (value == null)
+                return "-";
+            return ((TimeSpan)value).ToString();
+        }
+
+        public static string FormatTimeSpan(this TimeSpan value)
+        {
+            if (value.Days > 0)
+                return $"{value.Days} day{(value.Days > 1 ? "s": string.Empty)}";
+            if (value.Hours > 0)
+                return $"{value.Hours} hour{(value.Hours > 1 ? "s": string.Empty)}";
+            if (value.Minutes > 0)
+                return $"{value.Minutes} minute{(value.Minutes > 1 ? "s": string.Empty)}";
+            return value.ToString();
+
+        }
+
+        public static string FormatForCSV(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return "\"-\"";
+            }
+            else
+            {
+                //Remove any new line characters
+                value = value.Replace("\n", "\r");
+
+                return "\"" + value.Replace("\"", "\"\"") + "\"";
+            }
+
+        }
+
+        /// <summary>
+        /// Formats the length of the cable in mm and displays it in m to 2 decimal places.
+        /// </summary>
+        /// <param name="length">The length of the cable in mm.</param>
+        /// <returns>The formated length to display on labels.</returns>
+        public static string FormatCableLength(this decimal? length)
+        {
+            if (length == null)
+                return "-";
+
+            decimal meters = (decimal)length / 1000;
+
+            return $"{meters.ToString("#.##")}m";
+        }
+
+        public static int GetOrder<T>(this T enumValue)
+where T : struct, IConvertible
+        {
+            return ((T?)enumValue).GetOrder();
+        }
+
+        public static int GetOrder<T>(this T? enumValue)
+            where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                return 0;
+
+            if (enumValue == null)
+                return 0;
+
+            int order = 0;
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    order = ((DisplayAttribute)attrs[0]).Order;
+                }
+            }
+
+            return order;
+        }
+
+        public static string FormatName<T>(this T enumValue)
+where T : struct, IConvertible
+        {
+            return ((T?)enumValue).FormatName();
+        }
+
+        public static string FormatName<T>(this T? enumValue)
+            where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                return null;
+
+            if (enumValue == null)
+                return null;
+
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DisplayAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    var name = ((DisplayAttribute)attrs[0]).Name;
+                    if (!string.IsNullOrEmpty(name))
+                        return name;
+                }
+            }
+
+            return enumValue.ToString();
+        }
+
+
+        public static string FormatDescription<T>(this T enumValue)
+    where T : struct, IConvertible
+        {
+            return ((T?) enumValue).FormatDescription();
+        }
+
+        public static string FormatDescription<T>(this T? enumValue)
+            where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                return null;
+
+            if (enumValue == null)
+                return null;
+
+            var description = enumValue.ToString();
+            var fieldInfo = enumValue.GetType().GetField(enumValue.ToString());
+
+            if (fieldInfo != null)
+            {
+                var attrs = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    description = ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return description;
+        }
+    }
+}
---
+++ Data.Tracking.Blazor/Input/InputTimeSpan.razor.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Oarw.Data.Tracking.Blazor.Input
+{
+    public partial class InputTimeSpan : InputBase<TimeSpan?>
+    {
+        enum TimeResolution
+        {
+            [Display(Name = "Hour(s)")]
+            Hour,
+            [Display(Name = "Day(s)")]
+            Day,
+            [Display(Name = "Week(s)")]
+            Week,
+            [Display(Name = "Year(s)")]
+            Year
+        }
+
+        [Parameter]
+        public bool Small { get; set; }
+
+        [Parameter]
+        public bool ShowOrder { get; set; } = true;
+
+        [Parameter]
+        public string Placeholder { get; set; }
+
+        [Parameter]
+        public RenderFragment Append { get; set; }
+
+        private double? Offset
+        {
+            get
+            {
+                if (CurrentValue == null)
+                    return null;
+
+                switch(Resolution)
+                {
+                    case TimeResolution.Hour:
+                        return Math.Abs(((TimeSpan)CurrentValue).TotalHours);
+                    case TimeResolution.Day:
+                        return Math.Abs(((TimeSpan)CurrentValue).TotalDays);
+                    case TimeResolution.Week:
+                        return Math.Abs(((TimeSpan)CurrentValue).TotalDays / 7);
+                    case TimeResolution.Year:
+                        return Math.Abs(((TimeSpan)CurrentValue).TotalDays / 365);
+                }
+
+                return null;
+            }
+            set
+            {
+                switch(Resolution)
+                {
+                    case TimeResolution.Hour:
+                        CurrentValue = (value == null? null : TimeSpan.FromHours((double) value));
+                        break;
+                    case TimeResolution.Day:
+                        CurrentValue = (value == null ? null : TimeSpan.FromDays((double)value));
+                        break;
+                    case TimeResolution.Week:
+                        CurrentValue = (value == null ? null : TimeSpan.FromDays((double)value * 7));
+                        break;
+
+                    case TimeResolution.Year:
+                        CurrentValue = (value == null ? null : TimeSpan.FromDays((double)value * 365));
+                        break;
+                }
+                
+                ValueChanged.InvokeAsync(CurrentValue);
+            }
+        }
+
+        private TimeResolution Resolution { get; set; }
+
+        private bool Before
+        {
+            get { return CurrentValue < TimeSpan.Zero; }
+            set
+            {
+                if(Before != value)
+                {
+                    CurrentValue = -CurrentValue;
+                }                
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (CurrentValue != null)
+            {
+                if (((TimeSpan)CurrentValue).TotalDays % 365 == 0)
+                    Resolution = TimeResolution.Year;
+                else if (((TimeSpan)CurrentValue).TotalDays % 1 == 0)
+                    Resolution = TimeResolution.Day;
+                else if (((TimeSpan)CurrentValue).TotalHours % 1 == 0)
+                    Resolution = TimeResolution.Hour;
+
+                StateHasChanged();
+            }
+
+            await Task.CompletedTask;
+        }
+
+        protected override bool TryParseValueFromString(string value, out TimeSpan? result, [NotNullWhen(false)] out string validationErrorMessage)
+        {
+            result = TimeSpan.Parse(value);
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
