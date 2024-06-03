249e9908403724da7ce4b697346d8194468025f2
Oliver Waits <owaits@gmail.com>
2024-06-03 18:04:06 +0100

    Add DateTime validation attributes.

---
+++ Data.Tracking/Validation/DateTimeBeforeAttribute.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oarw.Data.Tracking.Validation
+{
+    /// <summary>
+    /// Provides validation based on whether the date and time is before another date and time property within the entity.
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.DataAnnotations.ValidationAttribute" />
+    public class DateTimeBeforeAttribute : ValidationAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DateTimeAfterAttribute"/> class.
+        /// </summary>
+        /// <param name="otherProperty">The other property.</param>
+        public DateTimeBeforeAttribute(string otherProperty) : base("The {0} must be after the {1}.")
+        {
+            this.OtherProperty = otherProperty;
+        }
+
+        /// <summary>
+        /// Gets or sets the other property name that will be used during validation.
+        /// </summary>
+        public string OtherProperty { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the display name of the other property.
+        /// </summary>
+        public string OtherPropertyDisplayName { get; set; }
+
+        /// <summary>
+        /// Applies formatting to an error message, based on the data field where the error occurred.
+        /// </summary>
+        /// <param name="name">The name to include in the formatted message.</param>
+        /// <returns>
+        /// An instance of the formatted error message.
+        /// </returns>
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(
+                CultureInfo.CurrentCulture,
+                base.ErrorMessageString,
+                name,
+                this.OtherPropertyDisplayName ?? this.OtherProperty);
+        }
+
+        /// <summary>
+        /// Validates the specified value with respect to the current validation attribute.
+        /// </summary>
+        /// <param name="value">The value to validate.</param>
+        /// <param name="validationContext">The context information about the validation operation.</param>
+        /// <returns>
+        /// An instance of the <see cref="T:System.ComponentModel.DataAnnotations.ValidationResult" /> class.
+        /// </returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (validationContext == null)
+            {
+                throw new ArgumentNullException("validationContext");
+            }
+
+            PropertyInfo otherProperty = validationContext.ObjectType.GetProperty(this.OtherProperty);
+            if (otherProperty == null)
+            {
+                return new ValidationResult(string.Format(CultureInfo.CurrentCulture, "Could not find a property named '{0}'.", this.OtherProperty));
+            }
+
+            DateTime? otherValue = (DateTime?) otherProperty.GetValue(validationContext.ObjectInstance);
+
+            //If either values are null allow it as not set yet
+            if(otherValue == null || value == null)
+                return ValidationResult.Success;
+
+            // check if this value is actually required and validate it
+            if (otherValue < (DateTime) value)
+            {
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
